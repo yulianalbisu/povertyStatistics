7a1c4949de0588075c27064473d22c63799f08bf
yuliana aldrich <yulianaaldrich@yulianas-MacBook-Air.local>
2023-09-17 16:09:05 -0400

    Adding css file, configured api key so I dont have to sign in all the time

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import ArcGISMap from "@arcgis/core/Map.js";
 import MapView from "@arcgis/core/views/MapView.js";
 
+import esriConfig from "@arcgis/core/config.js";
+
+const apiKey = 'AAPK2cfc253e947540b59ba889a00dcd4491EhtKpH8Sd1w3NFXMo4kAeEcS0yKJhVZYVCRbklbZFAzMfBORI37eiJbjLdJCP0-3'
+
+esriConfig.apiKey = apiKey;
+
 const map = new ArcGISMap({
-  basemap: "streets-vector"
+  basemap: "arcgis-dark-gray"
 });
 
 const view = new MapView({
